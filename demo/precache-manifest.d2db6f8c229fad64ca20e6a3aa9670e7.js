bce136bb39369c9a6fcc636bd8a6289eb7767ef4
Praveen Chinta <cvsnpraveen@gmail.com>
2019-11-16 23:13:38 +0530

    first commit

---
+++ demo/precache-manifest.d2db6f8c229fad64ca20e6a3aa9670e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "111b5fabb8d122dcd83bc8f3a8fe8ed2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e8a815a6a632b981955",
+    "url": "/static/css/main.3698205b.chunk.css"
+  },
+  {
+    "revision": "dbeaadbaaf0fe052e04c",
+    "url": "/static/js/2.3f10461d.chunk.js"
+  },
+  {
+    "revision": "3e8a815a6a632b981955",
+    "url": "/static/js/main.cf834f50.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  }
+]);
